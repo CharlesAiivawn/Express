58f050203107e299fba99bb974bf72c50822d37c
CharlesAiivawn <carlosech20052@gmail.com>
2025-11-25 07:19:40 -0600

    First commit

---
+++ express.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json());
+
+app.get('/alumnos', (req, res) => {
+  console.log(req.query);
+  res.send('23100151');
+})
+
+app.get('/docentes/:control'),(req, res)=>{
+  console.log(req.params);
+  res.send('L23100151');
+}
+
+app.get('/directivos',(req,res)=>{
+  console.log(req.query);
+  res.send('D23100151');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
